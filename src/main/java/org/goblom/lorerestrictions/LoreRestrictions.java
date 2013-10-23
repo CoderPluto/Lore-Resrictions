cff6f0145435ea6b445110c7d6346ca7b8b937ee
Bryan Larson <goblom@me.com>
2013-10-23 15:21:55 -0700

    Initial Commit... Does everything except for move handItem to inventory...
    WARNING: PLUGIN NOT TESTED

---
+++ src/main/java/org/goblom/lorerestrictions/LoreRestrictions.java
@@ -0,0 +1,59 @@
+
+package org.goblom.lorerestrictions;
+
+import java.util.List;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.Configuration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Goblom
+ */
+public class LoreRestrictions extends JavaPlugin {
+    
+    private LoreRestrictions instance;
+    private JavaPlugin plugin;
+    
+    public boolean isList = false;
+    public String prefix = ChatColor.DARK_GRAY + "[" + ChatColor.DARK_AQUA + "Lore Restrictions" + ChatColor.DARK_GRAY + "] " + ChatColor.RESET;
+    public LoreRestrictions getInstance() { return instance; }
+    public JavaPlugin getPlugin() { return plugin; }
+    
+    public void onEnable() {
+        saveDefaultConfig();
+        checkRestricted();
+        getServer().getPluginManager().registerEvents(new PlayerListener(this), plugin);
+    }
+    
+    public void onDisable() { HandlerList.unregisterAll(this); }
+    
+    public void checkRestricted() { 
+        if (getConfig().isList("Restricted-Words")) isList = true;
+        if (isList) {
+            for (String word : getConfig().getStringList("Restricted-Words")) {
+                if (getConfig().contains("Messages." + word)) return;
+                if (!getConfig().contains("Messages." + word)) {
+                    getConfig().set("Messages." + word, "You do not have permission to use this Item.");
+                    reloadConfig();
+                    
+                    /* Use addDefault */ //{
+                    /* Use addDefault */ //    Configuration defaults = getConfig().getDefaults();
+                    /* Use addDefault */ //    defaults.addDefault("Messages." + word, "You do not have permission to use this Item.");
+                    /* Use addDefault */ //    getConfig().setDefaults(defaults);
+                    /* Use addDefault */ //    reloadConfig();
+                    /* Use addDefault */ //}
+                }
+            }
+        } else {
+            getServer().getPluginManager().disablePlugin(this);
+            getLogger().severe("Lore Restrictions Disabled!");
+            getLogger().severe("Restricted-Words MUST BE A STRING LIST!");
+        }
+    }
+    public List<String> getRestrictedList() { return getConfig().getStringList("Restricted-Words"); }
+    public String getRestrictedWord() { return getConfig().getString("Restricted-Words"); }
+    public void sendMessage(Player player, String message) { player.sendMessage(prefix + ChatColor.translateAlternateColorCodes('&', message)); }
+}
---
+++ src/main/java/org/goblom/lorerestrictions/PlayerListener.java
@@ -0,0 +1,58 @@
+
+package org.goblom.lorerestrictions;
+
+import java.util.List;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerAnimationEvent;
+import org.bukkit.event.player.PlayerAnimationType;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+/**
+ *
+ * @author Goblom
+ */
+public class PlayerListener implements Listener {
+
+    private LoreRestrictions plugin;
+    public PlayerListener(LoreRestrictions instance) { plugin = instance; }
+
+    @EventHandler
+    private void onPlayerSwing(PlayerInteractEvent event) {
+        if (event.getItem() == null) return;
+        if (event.getItem().getType().equals(Material.AIR)) return;
+        ItemStack handItem = event.getItem();
+        Player player = event.getPlayer();
+        if (handItem.hasItemMeta()) {
+            ItemMeta handMeta = handItem.getItemMeta();
+            if (handMeta.hasLore()) {
+                List<String> lore = handMeta.getLore();
+                if (plugin.isList == false) {
+                    if (lore.contains(plugin.getRestrictedWord())) {
+                        if (player.hasPermission("lorerestriction." + plugin.getRestrictedWord())) return;
+                        if (!player.hasPermission("lorerestriction." + plugin.getRestrictedWord())) {
+                            //Figure out how to move handItem out of HotBar
+                            event.setCancelled(true);
+                           plugin.sendMessage(player, plugin.getConfig().getString("Messages." + plugin.getRestrictedWord()));
+                        }
+                    }
+                } else if (plugin.isList == false) {
+                    for (String string : plugin.getRestrictedList()) {
+                        if (lore.contains(string)) {
+                            if (player.hasPermission("lorerestriction." + string)) return;
+                            if (!player.hasPermission("lorerestriction." + string)) {
+                                //Figure out how to move handItem out of HotBar
+                                event.setCancelled(true);
+                                plugin.sendMessage(player, plugin.getConfig().getString("Messages." + string));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
